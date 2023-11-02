dd01f82725c9ab0a9afd519f77571ff2ca0381ee
Rupert Parry <rp@rupert.cloud>
2023-11-02 22:57:52 +1100

    Removed unnecessary type information

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,4 @@
 declare class ObservableElement extends HTMLElement {
-  attributeChangedCallback: Function;
-  connectedCallback: Function;
   observe(properties: string[], callback: Function): void;
 }
 
